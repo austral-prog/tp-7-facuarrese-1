312b29fdffa5abec27128db143c784321abc4293
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-10-01 16:41:18 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,14 @@
-def enumerate_list(list):
-    return list
+def enuerate_list(lista):
+    nl = []
+    for i in range(len(lista)):
+        nl.append(i)
+        nl.append(lista[i])
+        nl.append(",")
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+    nl=[]
+    for i in range(len(lista)):
+        r=lista[i][::-1]
+        nl.append(i)
+        nl.append(r)
+    print(nl)
