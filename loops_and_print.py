440d16fee51800f5e6b0eccb06ecea638e04654c
facuarrese <162734700+facuarrese@users.noreply.github.com>
2024-10-15 00:48:47 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,14 +1,22 @@
-def enuerate_list(lista):
-    nl = []
-    for i in range(len(lista)):
-        nl.append(i)
-        nl.append(lista[i])
-        nl.append(",")
+def enumerate_list(list):
+    lista_1 = []
+    lista_2 = []
+    for element in list:
+        if element != "":
+            lista_1.append(element)
+            print(lista_1)
+    for index in range(len(lista_1)):
+            lista_2.append(f"{index}. {lista_1[index]}")
+    return lista_2
 
-def enumerate_backwards(lista):
-    nl=[]
-    for i in range(len(lista)):
-        r=lista[i][::-1]
-        nl.append(i)
-        nl.append(r)
-    print(nl)
+
+def enumerate_backwards(list):
+    lista_1 = []
+    lista_2 = []
+    for element in list:
+        if element != "":
+             lista_1.append(element[::-1])
+             print(lista_1)
+    for index in range(len(lista_1)):
+         lista_2.append(f"{index}. {lista_1[index]}")
+    return lista_2
